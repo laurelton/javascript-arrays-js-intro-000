274d794a765922a3f0096ee396cea6aaaada3d31
Frank Rose <frankrosework@gmail.com>
2018-03-29 02:13:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 var addElementToBeginningOfArray = function(array, element) {
   return [element, array.slice()];
 }
+
+var destructivelyAddElementToBeginningOfArray = function(array, element) {
+  array.unshift(element);
+  return array;
+}
